6a5722ae858440c9b79adaf3d1be92d9ec368ac2
Suman Sharma <suman.sharma@infosys.com>
2020-08-06 12:41:55 +0530

    test app

---
+++ src/main/java/com/test/akaash/testApp/TestAppApplication.java
@@ -0,0 +1,20 @@
+package com.test.akaash.testApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TestAppApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestAppApplication.class);
+	}
+
+}
